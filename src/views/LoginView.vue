cb4a409e811a01693345d2e376eeff6a8772a1d5
Julian Hoffmann <wi21b050@technikum.at>
2023-09-27 20:30:03 +0200

    login form added

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,24 @@
 <template>
-    <div class="login">
-        <h1>Login</h1>
+    <div class="container d-flex justify-content-center align-items-center">
+        <div class="card login-card">
+            <div class="card-body">
+                <h2 class="card-title text-center">Login</h2>
+                <form>
+                    <div class="form-group">
+                        <label for="username">User Name/E-Mail</label>
+                        <input type="text" class="form-control" id="username" placeholder="Max/max.muster@gmail.com">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Passwort</label>
+                        <input type="password" class="form-control" id="password" placeholder="********" minlength="8">
+                    </div>
+                    <div class="button-container justify-content-evenly">
+                        <button type="submit" class="btn btn-primary card-button">Login</button>
+                        <button type="button" class="btn btn-primary card-button"
+                            onclick="location.href='#'">Registrieren</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 </template>
