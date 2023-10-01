0f60def3d89832bc2811200a43cbed338a3fd3e9
altink7 <kelmendialtin48@gmail.com>
2023-10-01 10:58:48 +0200

    music loop added

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,20 @@
       <router-view></router-view>
     </div>
     <FooterComponent />
+    <MusicPlayer ref="musicPlayer" />
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue';
 import FooterComponent from '@/components/FooterComponent.vue';
+import MusicPlayer from '@/components/MusicPlayer.vue';
 
 export default {
   components: {
     NavBar,
-    FooterComponent
+    FooterComponent,
+    MusicPlayer,
   }
 };
 </script>
---
+++ src/components/MusicPlayer.vue
@@ -0,0 +1,26 @@
+<template>
+    <audio id= "track" ref="audio" autoplay loop>
+      <source :src="currentTrack" type="audio/mpeg">
+    </audio>
+</template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        currentTrack: 'https://soundimage.org/wp-content/uploads/2016/10/Arcade-Fantasy.mp3',
+      };
+    },
+    methods: {
+      playMusic(src) {
+        this.$refs.audio.src = src;
+        this.$refs.audio.play();
+      },
+    },
+    changeTrack(track) {
+      this.currentTrack = track;
+      this.playMusic(this.currentTrack);
+    },
+  };
+  </script>
+  
