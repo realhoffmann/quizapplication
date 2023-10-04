902f4bd01f972a68b22ae364dd467d0f6daeebc2
Julian Hoffmann <wi21b050@technikum.at>
2023-10-04 19:14:59 +0200

    navbar collapse fixed

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,18 +2,41 @@
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
             <router-link to="/">
-                <a class="navbar-brand" href="#"><img src="../assets/logo.png" width="60" height="60">
+                <a class="navbar-brand"><img src="../assets/logo.png" width="60" height="60">
                 </a></router-link>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" @click="toggleNavbar">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse justify-content-evenly text-center" id="navbarSupportedContent">
-                <router-link to="/">Home</router-link>
-                <router-link to="/login">Login</router-link>
+            <div class="collapse navbar-collapse justify-content-evenly text-center" :class="{ 'show': isNavbarOpen }"
+                id="navbarSupportedContent">
+                <router-link to="/">Home</router-link><br>
+                <router-link to="/login">Login</router-link><br>
                 <router-link to="/about">About</router-link>
 
             </div>
         </div>
     </nav>
 </template>
+
+<!-- Navbar.vue -->
+
+  
+<script>
+export default {
+    data() {
+        return {
+            isNavbarOpen: false,
+        };
+    },
+    methods: {
+        toggleNavbar() {
+            this.isNavbarOpen = !this.isNavbarOpen;
+        },
+    },
+};
+</script>
+  
+<style scoped>
+/* Add custom CSS styles here */
+</style>
+  
