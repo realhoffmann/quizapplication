462a0fc3010d9eb07908598703e992767c385efc
Sebastian Gruenewald <wi21b080@technikum-wien.at>
2023-10-21 15:15:10 +0200

    made password fields required

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -24,7 +24,7 @@
           </div>
           <div class="mb-2">
             <label for="email">E-Mail-Adress</label>
-            <input v-model="user.email" type="email" class="form-control" id="email" placeholder="max.muster@gmail.com">
+            <input v-model="user.email" type="email" class="form-control" id="email" placeholder="max.muster@gmail.com" required>
           </div>
           <div>
             <label for="country">Country</label>
@@ -62,12 +62,12 @@
           <div class="mb-2">
             <label for="password">Password</label>
             <input v-model="user.password" type="password" class="form-control" id="password" placeholder="********"
-              minlength="8">
+              minlength="8" required>
           </div>
           <div class="mb-2">
             <label for="confirm-password">Confirm Password</label>
             <input v-model="confirmPassword" type="password" class="form-control" id="confirm-password"
-              placeholder="********" minlength="8">
+              placeholder="********" minlength="8" required>
           </div>
           <br>
           <div class="mb-2">
