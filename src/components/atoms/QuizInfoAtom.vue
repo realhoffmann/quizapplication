d1d626cd6de2baf5d79f8e79cd667e73b04ed35f
Sebastian Gruenewald <wi21b080@technikum-wien.at>
2023-11-15 10:18:27 +0100

    typo fix

--- src/components/atoms/QuizInfoAtom.vue
+++ src/components/atoms/QuizInfoAtom.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="ms-2 me-auto">
     <div class="fw-bold">Quiz ID: {{ quizId }}</div>
-    <div class="fw-bold">Duration: {{ duration }}s</div>
+    <div class="fw-bold">Duration: {{ duration }}</div>
     <div class="fw-bold">Category: {{ category }}</div>
   </div>
 </template>
